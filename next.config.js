63a9f22d23327c56f235a281e6a4d01f1bf52173
3-59z <141506604+3-59z@users.noreply.github.com>
2025-09-22 15:06:01 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // next export সক্রিয় করবে
+  output: 'export', // next export
 };
 
 module.exports = nextConfig;
