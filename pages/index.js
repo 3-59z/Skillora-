b7fdcda8d557abba0d282cbb6f7bdd56502c9a26
3-59z <141506604+3-59z@users.noreply.github.com>
2025-09-22 14:24:52 +0600

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{fontFamily:'sans-serif', padding:24}}>
+      <h1>Hello Vercel! My Next.js app is live 🚀</h1>
+      <p>This is the homepage (pages/index.js)</p>
+    </div>
+  );
+  }
